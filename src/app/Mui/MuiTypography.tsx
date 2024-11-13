fe74b437dd7ca06bb174019d472637a51e20229a
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-13 12:37:58 +0530

    Typography in MUI

---
+++ src/app/Mui/MuiTypography.tsx
@@ -0,0 +1,23 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography =()=>{
+    return (
+        <div>
+            <Typography variant="h1">Header</Typography>
+            <Typography variant="h2">Header</Typography>
+            <Typography variant="h3">Header</Typography>
+            <Typography variant="h4">Header</Typography>
+            <Typography variant="h5">Header</Typography>
+
+
+            <Typography variant="subtitle1">Subtitle1</Typography>
+            <Typography variant="subtitle2">Subtitle 2</Typography>
+
+
+<br />
+            <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo repudiandae sed dolorum sunt esse! Vero facere sed dolorem deleniti suscipit possimus quis eos quod at illo! Quasi a officiis veniam! 2</Typography><br />
+            <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nostrum fuga ut cumque? Est odit culpa asperiores magnam cupiditate sit explicabo tempore sint saepe vero, quas minus fuga accusantium excepturi? 2</Typography>
+        </div>
+    )
+   
+}
